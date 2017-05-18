868bef57c428750badf16300094d9af8fd68325c
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-05-18 11:36:41 +0900

    style cleanup
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- components/derasterizedTile.js
+++ components/derasterizedTile.js
@@ -7,7 +7,7 @@ export default ({ originColor, otherColor, size, origin, scaleFromGridCenter }) 
 	origin = scalePoint({ point: origin, scaleFromGridCenter })
 	const sizedUnit = size * UNIT
 
-	tile({ origin, size, originColor: otherColor, otherColor });
+	tile({ origin, size, originColor: otherColor, otherColor })
 
 	// BY AREA STYLE
 	let coordinates = [
@@ -42,50 +42,50 @@ export default ({ originColor, otherColor, size, origin, scaleFromGridCenter }) 
 	render({ color: originColor, coordinates })
 
 	//FROM "EXPERIMENTS" STYLE
-	// ctx.beginPath();
-	// ctx.moveTo(origin[ 0 ] * sizedUnit, origin[ 1 ] * sizedUnit);
-	// ctx.lineTo((origin[ 0 ] + Math.sqrt(6)/3) * sizedUnit, origin[ 1 ] * sizedUnit);
-	// ctx.lineTo(origin[ 0 ] * sizedUnit, (origin[ 1 ] + Math.sqrt(6)/3) * sizedUnit);
-	// ctx.closePath();
-	// ctx.fill();
+	// ctx.beginPath()
+	// ctx.moveTo(origin[ 0 ] * sizedUnit, origin[ 1 ] * sizedUnit)
+	// ctx.lineTo((origin[ 0 ] + Math.sqrt(6)/3) * sizedUnit, origin[ 1 ] * sizedUnit)
+	// ctx.lineTo(origin[ 0 ] * sizedUnit, (origin[ 1 ] + Math.sqrt(6)/3) * sizedUnit)
+	// ctx.closePath()
+	// ctx.fill()
 	//
-	// ctx.beginPath();
-	// ctx.moveTo((origin[ 0 ] + 1) * sizedUnit, (origin[ 1 ] + 1 - Math.sqrt(6)/3) * sizedUnit);
-	// ctx.lineTo((origin[ 0 ] + 1) * sizedUnit, (origin[ 1 ] + 1) * sizedUnit);
-	// ctx.lineTo((origin[ 0 ] + 1 - Math.sqrt(6)/3) * sizedUnit, (origin[ 1 ] + 1) * sizedUnit);
-	// ctx.closePath();
-	// ctx.fill();
+	// ctx.beginPath()
+	// ctx.moveTo((origin[ 0 ] + 1) * sizedUnit, (origin[ 1 ] + 1 - Math.sqrt(6)/3) * sizedUnit)
+	// ctx.lineTo((origin[ 0 ] + 1) * sizedUnit, (origin[ 1 ] + 1) * sizedUnit)
+	// ctx.lineTo((origin[ 0 ] + 1 - Math.sqrt(6)/3) * sizedUnit, (origin[ 1 ] + 1) * sizedUnit)
+	// ctx.closePath()
+	// ctx.fill()
 	//
-	// color = color === COLOR_A ? COLOR_B : COLOR_A;
-	// ctx.fillStyle = color;
+	// color = color === COLOR_A ? COLOR_B : COLOR_A
+	// ctx.fillStyle = color
 	//
-	// ctx.beginPath();
-	// ctx.moveTo(origin[ 0 ] * sizedUnit, origin[ 1 ] * sizedUnit);
-	// ctx.lineTo((origin[ 0 ] + Math.sqrt(2)/3) * sizedUnit, origin[ 1 ] * sizedUnit);
-	// ctx.lineTo(origin[ 0 ] * sizedUnit, (origin[ 1 ] + Math.sqrt(2)/3) * sizedUnit);
-	// ctx.closePath();
-	// ctx.fill();
+	// ctx.beginPath()
+	// ctx.moveTo(origin[ 0 ] * sizedUnit, origin[ 1 ] * sizedUnit)
+	// ctx.lineTo((origin[ 0 ] + Math.sqrt(2)/3) * sizedUnit, origin[ 1 ] * sizedUnit)
+	// ctx.lineTo(origin[ 0 ] * sizedUnit, (origin[ 1 ] + Math.sqrt(2)/3) * sizedUnit)
+	// ctx.closePath()
+	// ctx.fill()
 	//
-	// ctx.beginPath();
-	// ctx.moveTo((origin[ 0 ] + 1) * sizedUnit, (origin[ 1 ] + 1 - Math.sqrt(2)/3) * sizedUnit);
-	// ctx.lineTo((origin[ 0 ] + 1) * sizedUnit, (origin[ 1 ] + 1) * sizedUnit);
-	// ctx.lineTo((origin[ 0 ] + 1 - Math.sqrt(2)/3) * sizedUnit, (origin[ 1 ] + 1) * sizedUnit);
-	// ctx.closePath();
-	// ctx.fill();
+	// ctx.beginPath()
+	// ctx.moveTo((origin[ 0 ] + 1) * sizedUnit, (origin[ 1 ] + 1 - Math.sqrt(2)/3) * sizedUnit)
+	// ctx.lineTo((origin[ 0 ] + 1) * sizedUnit, (origin[ 1 ] + 1) * sizedUnit)
+	// ctx.lineTo((origin[ 0 ] + 1 - Math.sqrt(2)/3) * sizedUnit, (origin[ 1 ] + 1) * sizedUnit)
+	// ctx.closePath()
+	// ctx.fill()
 
 
 	//BY STRIPE WIDTH
-	// ctx.beginPath();
-	// ctx.moveTo(origin[ 0 ] * sizedUnit, origin[ 1 ] * sizedUnit);
-	// ctx.lineTo((origin[ 0 ] + (2/3)) * sizedUnit, origin[ 1 ] * sizedUnit);
-	// ctx.lineTo(origin[ 0 ] * sizedUnit, (origin[ 1 ] + (2/3)) * sizedUnit);
-	// ctx.closePath();
-	// ctx.fill();
+	// ctx.beginPath()
+	// ctx.moveTo(origin[ 0 ] * sizedUnit, origin[ 1 ] * sizedUnit)
+	// ctx.lineTo((origin[ 0 ] + (2/3)) * sizedUnit, origin[ 1 ] * sizedUnit)
+	// ctx.lineTo(origin[ 0 ] * sizedUnit, (origin[ 1 ] + (2/3)) * sizedUnit)
+	// ctx.closePath()
+	// ctx.fill()
 	//
-	// ctx.beginPath();
-	// ctx.moveTo((origin[ 0 ] + 1) * sizedUnit, (origin[ 1 ] + 1 - (2/3)) * sizedUnit);
-	// ctx.lineTo((origin[ 0 ] + 1) * sizedUnit, (origin[ 1 ] + 1) * sizedUnit);
-	// ctx.lineTo((origin[ 0 ] + 1 - (2/3)) * sizedUnit, (origin[ 1 ] + 1) * sizedUnit);
-	// ctx.closePath();
-	// ctx.fill();
+	// ctx.beginPath()
+	// ctx.moveTo((origin[ 0 ] + 1) * sizedUnit, (origin[ 1 ] + 1 - (2/3)) * sizedUnit)
+	// ctx.lineTo((origin[ 0 ] + 1) * sizedUnit, (origin[ 1 ] + 1) * sizedUnit)
+	// ctx.lineTo((origin[ 0 ] + 1 - (2/3)) * sizedUnit, (origin[ 1 ] + 1) * sizedUnit)
+	// ctx.closePath()
+	// ctx.fill()
 }
