bf718dd4cf4f16ab52c1208798c5cb9cabd1e013
Douglas <douglas.blumeyer@gmail.com>
2017-10-29 03:36:57 +0800

    typedef some effects

--- src/components/getDerasterizedByAreaStripePositions.ts
+++ src/components/getDerasterizedByAreaStripePositions.ts
@@ -1,8 +1,9 @@
 import { perStripe, StripePosition, to } from '../../../../src'
 
-const getDerasterizedByAreaStripePositions: () => StripePosition[] = () =>
-	perStripe({ getStripePosition: derasterizedByAreaStripePosition })
+const getDerasterizedByAreaStripePositions: () => StripePosition[] =
+	(): StripePosition[] => perStripe({ getStripePosition: derasterizedByAreaStripePosition })
 
-const derasterizedByAreaStripePosition: () => StripePosition = () => to.StripePosition(0)
+const derasterizedByAreaStripePosition: () => StripePosition =
+	(): StripePosition => to.StripePosition(0)
 
 export { getDerasterizedByAreaStripePositions }
