a9c84882406901fda6b07c03b8bb9894e30e7fba
Douglas <douglas.blumeyer@gmail.com>
2017-11-26 01:01:04 +0900

    respect import boundaries in unit tests, which actually requires a whole rethinking of modules to support spying, so now we no longer care from where a module is imported, because every module is imported with * as, and modules with a pseudo-default export call it main

--- pattern/stripe/getDerasterizedByAreaStripePositions.ts
+++ pattern/stripe/getDerasterizedByAreaStripePositions.ts
@@ -1,9 +1,9 @@
 import { perStripe, StripePosition, to } from '../../../../src'
 
 const getDerasterizedByAreaStripePositions: () => StripePosition[] =
-	(): StripePosition[] => perStripe({ getStripePosition: derasterizedByAreaStripePosition })
+	(): StripePosition[] => perStripe.main({ getStripePosition: derasterizedByAreaStripePosition })
 
 const derasterizedByAreaStripePosition: () => StripePosition =
 	(): StripePosition => to.StripePosition(0)
 
-export { getDerasterizedByAreaStripePositions }
+export { getDerasterizedByAreaStripePositions as main }
